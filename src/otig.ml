f624648292a51ec54c3ff355f527f67bdfdb2e6e
Kate <jp.deplaix@gmail.com>
2018-03-14 13:56:20 +0000

    Print the hash for each commits

--- src/otig.ml
+++ src/otig.ml
@@ -4,6 +4,7 @@ module Reference = Git_unix.FS.Reference
 module Ref = Git_unix.FS.Ref
 module Value = Git_unix.FS.Value
 module Commit = Git_unix.FS.Value.Commit
+module Hash = Git_unix.FS.Value.Commit.Hash
 
 let rec get_hash fs r =
   Ref.read fs r >>= function
@@ -31,15 +32,19 @@ let get_commit_short_message commit =
   let msgs = String.split_on_char '\n' msg in
   get_first_nonempty_string msgs
 
+let print_commit commit =
+  let hash = Hash.to_hex (Commit.digest commit) in
+  let hash = String.sub hash 0 (min 7 (String.length hash)) in
+  let msg = get_commit_short_message commit in
+  Printf.printf "%s - %s\n" hash msg
+
 let () =
   match
     Lwt_main.run begin
       Git_unix.FS.create () >>= fun fs ->
       get_hash fs Reference.head >>= fun hash ->
-      get_commits fs hash >>= fun commits ->
-      let msgs = List.map get_commit_short_message commits in
-      List.iter print_endline msgs;
-      Lwt.return (Ok ())
+      get_commits fs hash >|=
+      List.iter print_commit
     end
   with
   | Ok () -> ()
